297c1efd5e574c9dac2ddfe7661313435c74080d
Арман <@ARMAN>
2023-12-10 23:44:56 +0600

    Добавьте файлы проекта.

---
+++ prac15(3)/MyClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+public class MyClass
+{
+    private int privateField;
+
+    public MyClass()
+    {
+    }
+
+    public MyClass(int value)
+    {
+        privateField = value;
+    }
+
+    public int PublicProperty { get; set; }
+
+    private void PrivateMethod()
+    {
+        Console.WriteLine("Private method called.");
+    }
+
+    public void PublicMethod()
+    {
+        Console.WriteLine("Public method called.");
+    }
+}
---
+++ prac15(3)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Type myClassType = typeof(MyClass);
+
+        // Создание экземпляра MyClass без прямого вызова конструктора
+        object myClassInstance = Activator.CreateInstance(myClassType);
+
+        // Изменение значений свойств
+        myClassType.GetProperty("PublicProperty").SetValue(myClassInstance, 42);
+
+        // Вызов метода
+        myClassType.GetMethod("PublicMethod").Invoke(myClassInstance, null);
+
+        // Печать значения свойства для проверки изменений
+        Console.WriteLine($"PublicProperty после изменения: {myClassType.GetProperty("PublicProperty").GetValue(myClassInstance)}");
+    }
+}
